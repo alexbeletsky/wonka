58975dc302b1e72baf134fbc189c7256db3ee3a5
alexander.beletsky <alexander.beletsky@gmail.com>
2012-04-07 22:57:20 +0300

    refactoring, moved to separate file

---
+++ src/Wonka.Core/Github/Model/Object.cs
@@ -0,0 +1,9 @@
+﻿namespace Wonka.Core.Github.Model
+{
+    public class Object
+    {
+        public string Type { get; set; }
+        public string Sha { get; set; }
+        public string Url { get; set; }
+    }
+}
