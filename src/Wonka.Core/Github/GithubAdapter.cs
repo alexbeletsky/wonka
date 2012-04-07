6d734aae8270074fe63ab01b1ea90844b4fc9710
alexander.beletsky <alexander.beletsky@gmail.com>
2012-04-07 22:56:50 +0300

    get all references api method added

---
+++ src/Wonka.Core/Github/GithubAdapter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+using Wonka.Core.Github.Model;
+
+namespace Wonka.Core.Github
+{
+    public class GithubAdapter
+    {
+        private readonly string _user;
+        private readonly string _repo;
+        
+        private const string ApiBaseUri = "https://api.github.com";
+
+        public GithubAdapter(string user, string repo)
+        {
+            _user = user;
+            _repo = repo;
+        }
+
+        public IList<Reference> GetAllReferences()
+        {
+            using (var client = new WebClient())
+            {
+                var response = client.DownloadString(RefsUri);
+
+                return JsonConvert.DeserializeObject<IList<Reference>>(response);
+            }
+        }
+
+        protected Uri RefsUri { get { return new Uri(string.Format("{0}/repos/{1}/{2}/git/refs", ApiBaseUri, _user, _repo));}}
+    }
+}
---
+++ src/Wonka.Core/Github/Model/Reference.cs
@@ -0,0 +1,16 @@
+﻿namespace Wonka.Core.Github.Model
+{
+    public class Reference
+    {
+        public string Ref { get; set; }
+        public string Url { get; set; }
+        public Object Object { get; set; }
+    }
+
+    public class Object
+    {
+        public string Type { get; set; }
+        public string Sha { get; set; }
+        public string Url { get; set; }
+    }
+}
